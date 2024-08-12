9169c67899bf34f2364b6799bc62faf1419086e4
Abhijana <abhijanaramanda@gmail.com>
2024-08-12 18:35:21 +0800

    my solution to groupAnagram (beats 35.87%)

--- arrays_hashing/src/group_anagrams.rs
+++ arrays_hashing/src/group_anagrams.rs
@@ -24,7 +24,27 @@
 struct Solution;
 impl Solution {
     pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
-        todo!()
+        if strs.len() == 1 {
+            return vec![strs];
+        }
+
+        use std::collections::HashMap;
+
+        let mut grouped_word: HashMap<String, Vec<String>> = HashMap::new();
+
+        for word in strs {
+            let mut sorted_word_chars: Vec<char> = word.chars().collect();
+            sorted_word_chars.sort_by(|lhs, rhs| lhs.cmp(rhs));
+
+            let key: String = sorted_word_chars.into_iter().collect();
+            grouped_word
+                .entry(key)
+                .and_modify(|words| words.push(word.clone()))
+                .or_insert(vec![word]);
+        }
+
+        let grouped_word: Vec<Vec<String>> = grouped_word.into_iter().map(|(_, v)| v).collect();
+        grouped_word
     }
 }
 
@@ -38,6 +58,7 @@ mod test {
             .into_iter()
             .map(|s| s.to_owned())
             .collect();
+        // Since the order doesnt matter, the assertion may fail
         assert_eq!(
             Solution::group_anagrams(strs),
             vec![
@@ -55,7 +76,7 @@ mod test {
     }
 
     #[test]
-    fn test_case_2() {
+    fn test_case_3() {
         let strs: Vec<String> = ["a".to_owned()].to_vec();
         assert_eq!(Solution::group_anagrams(strs), vec![vec!["a".to_owned()]]);
     }
